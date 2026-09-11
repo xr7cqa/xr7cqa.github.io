df809a0de265ae4ab9445abf10c696330bdd4944
xr7cqa <zaydry7un@gmail.com>
2026-09-12 01:28:42 +0300

    fix: publish full muallim-pro assets (unsplit)

--- muallim-pro/assets/main-CiPoXCTF.js
+++ muallim-pro/assets/main-CiPoXCTF.js
@@ -1,5 +1,1 @@
-const parts=await Promise.all([0,1,2,3].map(i=>fetch(new URL(`main-CiPoXCTF.p${i}.txt`, import.meta.url)).then(r=>r.text())));
-let code=parts.join('');
-code=code.replaceAll('./prompts-COg7M83J.js', new URL('./prompts-COg7M83J.js', import.meta.url).href);
-const blob=new Blob([code],{type:'text/javascript'});
-await import(/* @vite-ignore */ URL.createObjectURL(blob));
+PLACEHOLDER
